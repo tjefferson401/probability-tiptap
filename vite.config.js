357a0aaa7d499e609bd17c9576748bc2db6251b2
TJ Jefferson <tjefferson4040@gmail.com>
2024-07-28 15:51:01 -0700

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: "/probability-tiptap/",
   esbuild: {
     loader: 'jsx', // Ensure esbuild handles JSX syntax
     include: [
